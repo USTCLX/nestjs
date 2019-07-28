4d30134fdeae3cb8655ba3164621e774a949035c
lx <sa514075@mail.ustc.edu.cn>
2019-07-28 22:50:47 +0800

    udpate - first lesson

---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: lixiang 
+ * @Date: 2019-07-28 16:04:43 
+ * @Last Modified by: lixiang
+ * @Last Modified time: 2019-07-28 16:06:41
+ */
+
+export class CreateCatDto {
+  readonly name: string;
+  readonly age: number;
+  readonly breed: string;
+}
